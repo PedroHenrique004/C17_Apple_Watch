0f983514835f2fb0fd93be1c362703da547342a3
Pedro Santos <pedrohenriqueph004@gmail.com>
2026-08-19 14:45:40 -0300

    Initial Commit

---
+++ Respirapp Watch App/RespirappApp.swift
@@ -0,0 +1,17 @@
+//
+//  RespirappApp.swift
+//  Respirapp Watch App
+//
+//  Created by Pedro Santos on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Respirapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Respirapp Watch AppTests/Respirapp_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  Respirapp_Watch_AppTests.swift
+//  Respirapp Watch AppTests
+//
+//  Created by Pedro Santos on 19/08/26.
+//
+
+import Testing
+@testable import Respirapp_Watch_App
+
+struct Respirapp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
